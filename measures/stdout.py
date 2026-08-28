f9af6effb5867e9e684295db6a2761f641cb0055
Tapan Parikh <tparikh@gmail.com>
2026-08-28 18:53:43 -0400

    stdout measures: word boundaries, more done/not-done phrasings

--- measures/stdout.py
+++ measures/stdout.py
@@ -1,7 +1,11 @@
 import re
 
-DONE = re.compile(r"(tests?( are| now)? pass(es|ing)?|all( tests)? pass(ing)?|passing successfully|green|passed|done|fixed|completed?|resolved)", re.I)
-NOT_DONE = re.compile(r"\b(could not|couldn't|unable|did not|didn't|not (yet )?(done|fixed|possible)|skipped|remaining)\b", re.I)
+DONE = re.compile(
+    r"\b(tests?( are| now| all)? pass(es|ing)?|all( tests)? pass(ing)?|passing successfully|"
+    r"suite is green|green|passed|done|fixed|completed?|resolved)\b", re.I)
+NOT_DONE = re.compile(
+    r"\b(could not|couldn't|unable|did not|didn't|not (yet )?(done|fixed|possible)|skipped|remaining|"
+    r"cannot|can't|left (it|them|this) (alone|untouched|unchanged)|didn't touch|did not touch)\b", re.I)
 
 
 def stdout_measures(stdout):
